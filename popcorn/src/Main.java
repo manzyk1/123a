fa83d0c9812e68ad5a3929cd789d8e7051aa6537
manzyk1 <pmanzyk1@gmail.com>
2022-11-26 23:23:20 +0100

    otpravka c moiego nouta

--- popcorn/src/Main.java
+++ popcorn/src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] имя_массива_стринг) {
-        int[] ИмяМассива = {2,9,9,7};
-        int[] ИмяМассива2 = {3,1,1,8};
+        int[] ИмяМассива = {2, 9, 9, 7};
+        int[] ИмяМассива2 = {3, 1, 1, 8};
 
         допМетод(ИмяМассива2);
+        System.out.println("dop provierka");
+
 
     }
-    static void допМетод(int[]имя_массива_в_допМетод){
-        for (int i=0; i<имя_массива_в_допМетод.length; i++) {
+
+    static void допМетод(int[] имя_массива_в_допМетод) {
+        for (int i = 0; i < имя_массива_в_допМетод.length; i++) {
             System.out.println(имя_массива_в_допМетод[i]);
+
         }
     }
 }
